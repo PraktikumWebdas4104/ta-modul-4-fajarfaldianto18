46cf90856f6628a4dfe9c8a3705698dbd15fe519
Fajar Febbi Aldianto <43521209+fajarfaldianto18@users.noreply.github.com>
2018-09-30 20:36:14 +0700

    formawal.php
    login

--- formawal.php
+++ formawal.php
@@ -1,5 +1,23 @@
-<?php
+<?php 
+	
+ ?>
 
+<h1><center>Login</center></h1>
+<form action="hobi.php" method="GET">
 
+<table align="center">
+	<tr>
+		<td>ID :</td>  
+		<td><input type="text" name="id"></td>
+	</tr>
+	<tr>	
+		<td>Password :</td> 
+		<td><input type="password" name="password"></td>
+	</tr>
+	<tr>	
+		<td><input type="submit" name="submit" value="kirim"></td>
+	</tr>
 
-?>
+</table>
+	
+</form>
